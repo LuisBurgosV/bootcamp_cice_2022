c97e291cbcad63bfe6b07fde55cf5bafc5a0d564
LuisBurgosV <luis.burgos.vidales@gmail.com>
2022-01-16 03:28:45 -0800

    Creación de Proyecto
    Creacion de mi primera app

---
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Luis Burgos on 16/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
