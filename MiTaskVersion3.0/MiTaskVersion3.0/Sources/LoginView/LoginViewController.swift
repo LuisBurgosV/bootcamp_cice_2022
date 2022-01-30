6fbaee6b244490ce4f436a7f70d1725c6d2c36de
LuisBurgosV <luis.burgos.vidales@gmail.com>
2022-01-30 09:40:17 -0800

    Colores, login terminado
    Feliz domingo

--- MiTaskVersion3.0/MiTaskVersion3.0/Sources/LoginView/LoginViewController.swift
+++ MiTaskVersion3.0/MiTaskVersion3.0/Sources/LoginView/LoginViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    //MARK: - Variables globales
+    var usuarioLogado = false
+    
     //MARK: - IBOutlets
     @IBOutlet weak var contentViewUsername: UIView!
     @IBOutlet weak var contentViewPassword: UIView!
@@ -19,32 +22,61 @@ class LoginViewController: UIViewController {
     
     //MARK: - IBActions
     @IBAction func rememberMeAction(_ sender: Any) {
+        if datosCompletados() {
+            self.usuarioLogado = true
+            Utils.Constantes().kPrefs.setValue(self.usernameTF.text, forKey: Utils.Constantes().kUsername)
+            Utils.Constantes().kPrefs.setValue(self.passwordTF.text, forKey: Utils.Constantes().kPassword)
+            Utils.Constantes().kPrefs.setValue(self.usuarioLogado, forKey: Utils.Constantes().kUsuarioLogado)
+            self.configuracionUI(color: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1))
+        }else{
+            self.present(Utils.muestraAlerta(titulo: "Hey!", mensaje: "Rellena el username y password", completionHandler: nil), animated: true, completion: nil)
+        }
     }
     
     @IBAction func loginAction(_ sender: Any) {
+        if datosCompletados() {
+            self.borrarDatosFormulario()
+        }else{
+            self.present(Utils.muestraAlerta(titulo: "Hey!", mensaje: "Rellena el username y password", completionHandler: nil), animated: true, completion: nil)
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.configuracionUI()
+        self.configuracionUI(color: #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1))
 
         // Do any additional setup after loading the view.
     }
     
     //MARK: - Metodos privados
-    private func configuracionUI() {
+    private func configuracionUI(color: UIColor) {
         self.contentViewUsername.backgroundColor = .clear
         self.contentViewUsername.layer.cornerRadius = 37
         self.contentViewUsername.layer.borderWidth = 2
-        self.contentViewUsername.layer.borderColor = #colorLiteral(red: 1, green: 0.2827598444, blue: 0.25824927, alpha: 1)
+        self.contentViewUsername.layer.borderColor = color.cgColor
         
         self.contentViewPassword.backgroundColor = .clear
         self.contentViewPassword.layer.cornerRadius = 37
         self.contentViewPassword.layer.borderWidth = 2
-        self.contentViewPassword.layer.borderColor = #colorLiteral(red: 1, green: 0.2827598444, blue: 0.25824927, alpha: 1)
+        self.contentViewPassword.layer.borderColor = color.cgColor
         
-        self.loginBtn.layer.cornerRadius = 22
+        self.loginBtn.layer.cornerRadius = 23
     }
-
-
+    
+    private func datosCompletados() -> Bool {
+        return !(self.usernameTF.text?.isEmpty ?? false) && !(self.passwordTF.text?.isEmpty ?? false)
+    }
+    
+    private func borrarDatosFormulario() {
+        self.usernameTF.text = ""
+        self.passwordTF.text = ""
+        self.mostrarHomeTabBar()
+    }
+    
+    private func mostrarHomeTabBar() {
+        let vc = HomeTabBarViewCoordinator.homeViewController()
+        vc.modalTransitionStyle = .coverVertical
+        vc.modalPresentationStyle = .fullScreen
+        self.present(vc, animated: true, completion: nil)
+    }
 }
---
+++ MiTaskVersion3.0/MiTaskVersion3.0/Utils+Helpers/Utils.swift
@@ -0,0 +1,33 @@
+//
+//  Utils.swift
+//  MiTaskVersion3.0
+//
+//  Created by Luis Burgos on 30/1/22.
+//
+
+import UIKit
+
+class Utils {
+    struct Constantes {
+        let kUsername = "USERNAME"
+        let kPassword = "PASSWORD"
+        let kUsuarioLogado = "LOGADO"
+        let kPrefs = UserDefaults.standard
+    }
+    
+    static func muestraAlerta(titulo: String, mensaje: String, completionHandler: ((UIAlertAction) -> Void)?) -> UIAlertController {
+        let alertVC = UIAlertController(title: titulo, message: mensaje, preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: completionHandler))
+        return alertVC
+    }
+}
+
+protocol ReuseIdentifierProtocol: AnyObject {
+    static var defaultReuseIdentifier: String { get }
+}
+
+extension ReuseIdentifierProtocol where Self: UIView {
+    static var defaultReuseIdentifier: String {
+        return NSStringFromClass(self).components(separatedBy: ".").last!
+    }
+}
