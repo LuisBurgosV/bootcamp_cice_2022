b579eb2e0433def6379993eeb7c60cb64c1b23b0
LuisBurgosV <luis.burgos.vidales@gmail.com>
2022-02-18 12:54:27 -0800

    terminado lo de ls splash y pasamos a templates

--- CiceMusic/CiceMusic/Sources/SplashView/SplashPresenter.swift
+++ CiceMusic/CiceMusic/Sources/SplashView/SplashPresenter.swift
@@ -10,14 +10,17 @@ import Foundation
 //Input del presenter
 protocol SplashPresenterInputProtocol {
     func fettchDataFromWebService()
+    func showHomeTabBar()
 }
 
 //Output del interactor
 protocol SplashInteractorOutputProtocol {
-    
+    func setDataFromWebInteractor(data: [ResultMusic]?)
 }
 
 final class SplashPresenter: BasePresenter<SplashPresenterOutputProtocol, SplashInteractorInputProtocol, SplashRouterInputProtocol> {
+    
+    var dataSourceMusic: [ResultMusic] = []
     
 }
 
@@ -26,9 +29,20 @@ extension SplashPresenter: SplashPresenterInputProtocol {
     func fettchDataFromWebService() {
         self.interactor?.fettchDataFromWebServiceInteractor()
     }
+    
+    func showHomeTabBar() {
+        self.router?.showHomeTabBarRouter(dataSource: self.dataSourceMusic)
+    }
 }
 
 //Output del interactor
 extension SplashPresenter: SplashInteractorOutputProtocol {
-    
+    func setDataFromWebInteractor(data: [ResultMusic]?) {
+        guard let dataUnw = data else { return }
+        
+        self.dataSourceMusic.removeAll()
+        self.dataSourceMusic = dataUnw
+        self.viewController?.launchAnimation()
+        
+    }
 }
--- CiceMusic/CiceMusic/Sources/SplashView/SplashViewController.swift
+++ CiceMusic/CiceMusic/Sources/SplashView/SplashViewController.swift
@@ -9,10 +9,18 @@ import UIKit
 
 //Output del presenter
 protocol SplashPresenterOutputProtocol {
-    
+    func launchAnimation()
 }
 
 class SplashViewController: BaseView<SplashPresenterInputProtocol> {
+    
+    //MARK: - Variables Globales
+    var viewAnimator: UIViewPropertyAnimator?
+    var unblockedGR = Timer()
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var customSplashImageView: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,9 +28,29 @@ class SplashViewController: BaseView<SplashPresenterInputProtocol> {
 
         // Do any additional setup after loading the view.
     }
+    
+    @objc
+    func automaticHandler() {
+        self.viewAnimator = UIViewPropertyAnimator(duration: 0.5, curve: .easeInOut, animations: nil)
+        self.viewAnimator?.addAnimations {
+            self.customSplashImageView.transform = CGAffineTransform(scaleX: 50, y: 50)
+            self.customSplashImageView.alpha = 0
+        }
+        self.viewAnimator?.startAnimation()
+        self.viewAnimator?.addCompletion({ _ in
+            self.presenter?.showHomeTabBar()
+        })
+    }
 }
 
 //Output del presenter
 extension SplashViewController: SplashPresenterOutputProtocol {
-    
+    func launchAnimation() {
+        self.viewAnimator = UIViewPropertyAnimator(duration: 1.0, curve: .easeInOut, animations: nil)
+        self.viewAnimator?.addAnimations {
+            self.customSplashImageView.transform = CGAffineTransform(scaleX: 0.7, y: 0.7)
+            self.unblockedGR = Timer.scheduledTimer(timeInterval: 1.5, target: self, selector: #selector(self.automaticHandler), userInfo: nil, repeats: false)
+        }
+        self.viewAnimator?.startAnimation()
+    }
 }
