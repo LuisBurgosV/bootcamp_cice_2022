39bf9cb9d99ff03a7491c41c03ab082ce5936bcf
LuisBurgosV <luis.burgos.vidales@gmail.com>
2022-02-19 04:57:25 -0800

    templates de music, podcast y books

---
+++ CiceMusic/CiceMusic/Sources/HomeTabBar/HomeTabBarCoordinator.swift
@@ -0,0 +1,40 @@
+//
+//  HomeTabBarCoordinator.swift
+//  CiceMusic
+//
+//  Created by Luis Burgos on 19/2/22.
+//
+
+import UIKit
+
+final class HomeTabBarCoordinator {
+    static func tabBarController(dto: HomeTabBarCoordinatorDTO? = nil) -> UITabBarController {
+        
+        let tbc = HomeTabBarViewController()
+        
+        //Controllers
+        let musicVC = MusicCoordinator.navigation(dto: MusicCoordinatorDTO(model: dto?.data))
+        let podcastVC = PodcastCoordinator.navigation()
+        let booksVC = BookCoordinator.navigation()
+        
+        //Icons
+        let iconMusic = UITabBarItem(title: "Music", image: UIImage(systemName: "music.note.list"), selectedImage: UIImage(systemName: "music.note.list"))
+        
+        let iconPodcast = UITabBarItem(title: "Music", image: UIImage(systemName: "music.note.house.fill"), selectedImage: UIImage(systemName: "music.note.house.fill"))
+        
+        let iconBooks = UITabBarItem(title: "Music", image: UIImage(systemName: "book"), selectedImage: UIImage(systemName: "book.fill"))
+        
+        //Asignamos el icono al controlador
+        musicVC.tabBarItem = iconMusic
+        podcastVC.tabBarItem = iconPodcast
+        booksVC.tabBarItem = iconBooks
+        
+        tbc.viewControllers = [musicVC, podcastVC, booksVC]
+                
+        return tbc
+    }
+}
+
+struct HomeTabBarCoordinatorDTO {
+    var data: [ResultMusic]
+}
--- CiceMusic/CiceMusic/Sources/SplashView/SplashRouter.swift
+++ CiceMusic/CiceMusic/Sources/SplashView/SplashRouter.swift
@@ -19,6 +19,9 @@ final class SplashRouter: BaseRouter<SplashViewController> {
 //Input del router
 extension SplashRouter: SplashRouterInputProtocol {
     func showHomeTabBarRouter(dataSource: [ResultMusic]) {
-        debugPrint(dataSource)
+        let vc = HomeTabBarCoordinator.tabBarController(dto: HomeTabBarCoordinatorDTO(data: dataSource))
+        vc.modalTransitionStyle = .crossDissolve
+        vc.modalPresentationStyle = .fullScreen
+        self.viewController?.present(vc, animated: true, completion: nil)
     }
 }
