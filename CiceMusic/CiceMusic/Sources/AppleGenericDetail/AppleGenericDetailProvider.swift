bf9072bdf6fdf047dbaa67ef90efc5f1ebab11b7
LuisBurgosV <luis.burgos.vidales@gmail.com>
2022-02-25 11:37:54 -0800

    detalles de la musica y apple

--- CiceMusic/CiceMusic/Sources/AppleGenericDetail/AppleGenericDetailProvider.swift
+++ CiceMusic/CiceMusic/Sources/AppleGenericDetail/AppleGenericDetailProvider.swift
@@ -23,23 +23,35 @@ POSSIBILITY OF SUCH DAMAGE.
 */
 
 import Foundation
-import UIKit
 
-//Input del router
-protocol MusicRouterInputProtocol {
-    func didSelectRowRouter(data: GenericResult)
-}
-
-final class MusicRouter: BaseRouter<MusicViewController> {
+//Input protocol
+protocol AppleGenericDetailProviderInputProtocol {
     
+    func fetchData(completionHandler: @escaping (Result<AppServerModel, NetworkError>) -> Void)
 }
 
-//Input del router
-extension MusicRouter: MusicRouterInputProtocol {
-    func didSelectRowRouter(data: GenericResult) {
-        DispatchQueue.main.async {
-            let vc = AppleGenericDetailCoordinator.view(dto: AppleGenericDetailCoordinatorDTO(dataModel: data))
-            self.viewController?.show(vc, sender: nil)
+final class AppleGenericDetailProvider: AppleGenericDetailProviderInputProtocol {
+    
+    let networkService: NetworkServiceProtocol = NetworkService()
+    
+    func fetchData(completionHandler: @escaping (Result<AppServerModel, NetworkError>) -> Void) {
+        self.networkService.requestGeneric(requestPayload: AppleGenericDetailRequestDTO.requestData(numeroItems: "10"),
+                                           entityClass: AppServerModel.self) { [weak self] (result) in
+            guard self != nil else { return }
+            guard let resultUnw = result else { return }
+            completionHandler(.success(resultUnw))
+        } failure: { (error) in
+            completionHandler(.failure(error))
         }
+    }
+}
+
+struct AppleGenericDetailRequestDTO {
+    
+    static func requestData(numeroItems: String) -> RequestDTO {
+        let argument: [CVarArg] = [numeroItems]
+        let urlComplete = String(format: URLEnpoint.apps, arguments: argument)
+        let request = RequestDTO(arrayParams: nil, method: .get, endpoint: urlComplete)
+        return request
     }
 }
