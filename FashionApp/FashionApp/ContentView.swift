922b7856d1a6aed1f78a1f5d070fe1714d3cd138
LuisBurgosV <luis.burgos.vidales@gmail.com>
2022-03-11 20:23:31 +0100

    Animaciones y cositas con swift UI

--- FashionApp/FashionApp/ContentView.swift
+++ FashionApp/FashionApp/ContentView.swift
@@ -77,6 +77,50 @@ struct ShapesView: View {
     }
 }
 
+struct HorizontalStackView: View {
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: true) {
+            VStack {
+                HeaderComponents()
+                HStack(spacing: 20) {
+                    Text("Leading")
+                    Text("Middle")
+                    Text("Trailing")
+                }
+                .padding()
+                .border(Color.orange)
+                
+                
+                HStack {
+                    Image(systemName: "1.circle").foregroundColor(.yellow)
+                    Image(systemName: "2.circle").foregroundColor(.blue)
+                    Image(systemName: "3.circle").foregroundColor(.red)
+                }
+                .padding()
+                .font(.largeTitle)
+                
+                HStack {
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 100) {
+                            Button(action: {}) {
+                                Image(systemName: "a.circle.fill").foregroundColor(.red)
+                            }
+                            Image(systemName: "b.circle.fill").foregroundColor(.yellow)
+                            Image(systemName: "c.circle.fill").foregroundColor(.black)
+                            Image(systemName: "d.circle.fill").foregroundColor(.blue)
+                            Image(systemName: "f.circle.fill").foregroundColor(.gray)
+                            Image(systemName: "f.circle.fill").foregroundColor(.green)
+                        }
+                        .padding()
+                    }
+                }
+                .font(.largeTitle)
+                .background(Color.clear)
+            }
+        }
+    }
+}
+
 struct VerticalStackView: View {
     var body: some View {
         ScrollView(.vertical, showsIndicators: true) {
@@ -118,24 +162,217 @@ struct VerticalStackView: View {
                         .foregroundColor(.blue)
                 )
                 .padding()
-                
-                HeaderComponents()
-                HeaderComponents()
-                HeaderComponents()
-                HeaderComponents()
-                HeaderComponents()
-                HeaderComponents()
-                HeaderComponents()
             }
         }
     }
 }
 
+struct AlignmentHStackView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            HeaderComponents()
+            
+            HStack {
+                Rectangle()
+                    .foregroundColor(.orange)
+                    .frame(width: 25)
+                Text("Leading")
+                Spacer()
+                Text("Center")
+                Spacer()
+                Text("Trailing")
+                    .padding(.trailing, 5)
+            }
+            .border(Color.orange)
+            
+            HStack(alignment: .top) {
+                Rectangle()
+                    .foregroundColor(.orange)
+                    .frame(width: 25)
+                Text("Leading").padding(.top, 5)
+                Spacer()
+                Text("Center").padding(.top, 5)
+                Spacer()
+                Text("Trailing").padding([.trailing, .top], 5)
+            }
+            .border(Color.orange)
+            
+            HStack(alignment: .bottom) {
+                Rectangle()
+                    .foregroundColor(.orange)
+                    .frame(width: 25)
+                Text("Leading").padding(.bottom, 5)
+                Spacer()
+                Text("Center").padding(.bottom, 5)
+                Spacer()
+                Text("Trailing").padding([.trailing, .bottom], 5)
+            }
+            .border(Color.orange)
+        }
+    }
+}
+
+struct HStackPriorityView: View {
+    
+    var textViewAux: some View {
+        Text(".layoutPriority(1) asigna hacia donde va el equilibrio de la vista")
+            .font(.largeTitle)
+            .padding(.horizontal)
+            .background(Color.orange)
+    }
+    
+    var priorityHStack: some View {
+        VStack {
+            HStack {
+                Text("Swift UI")
+                    .font(.title)
+                    .lineLimit(1)
+                Image("usuario1")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(Circle())
+                Text("Equilibrio de la vista")
+                    .font(.title2)
+                    .layoutPriority(1)
+            }
+            .padding(.horizontal)
+            
+            HStack {
+                Text("Swift UI")
+                    .font(.title)
+                    //.lineLimit(1)
+                    .layoutPriority(1)
+                Image("usuario1")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(Circle())
+                Text("Equilibrio de la vista")
+                    .font(.title2)
+                    .lineLimit(1)
+            }
+            .padding(.horizontal)
+        }
+    }
+    
+    @ViewBuilder
+    func showNewText() -> some View {
+        VStack {
+            Text("Toma ya!")
+        }
+    }
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 20) {
+            HeaderComponents()
+            textViewAux
+            priorityHStack
+            showNewText()
+        }
+    }
+}
+
+struct IntroZStackView: View {
+    var body: some View {
+        
+        Group {
+            ZStack(alignment: .topTrailing) {
+                
+                VStack(spacing: 20) {
+                    Spacer()
+                    HeaderComponents()
+                    Spacer()
+                }
+                
+                Text("Esta es una ZStack")
+                    .padding(.trailing, 20)
+                
+            }
+            .frame(maxWidth: .infinity)
+            .background(Color.gray)
+            .edgesIgnoringSafeArea(.all)
+        }
+    }
+}
+
+struct BasicChangedCircle: View {
+    
+    @State private var change = false
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Circle()
+                .foregroundColor(.orange)
+                .frame(width: 100, height: 100)
+                .offset(x: self.change ? 120 : 0, y: self.change ? 300 : 0)
+                .animation(.easeInOut)
+            
+            Spacer()
+            
+            Button("Change") {
+                self.change.toggle()
+            }
+        }
+    }
+}
+
+struct CurvesAnimationsView: View {
+    
+    @State private var move = false
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Animacion con curvas")
+                .font(.largeTitle)
+            Group {
+                Text("1")
+                GeometryReader { proxy in
+                    Circle()
+                        .frame(width: 50, height: 50)
+                        .foregroundColor(.orange)
+                        .position(x: self.move ? (proxy.size.width - 40) : 40, y: 40)
+                        .animation(.easeInOut(duration: 2))
+                }
+                
+                Text("2")
+                GeometryReader { proxy in
+                    Circle()
+                        .frame(width: 50, height: 50)
+                        .foregroundColor(.orange)
+                        .position(x: self.move ? (proxy.size.width - 40) : 40, y: 40)
+                        .animation(.linear(duration: 2))
+                }
+                
+                Text("3")
+                GeometryReader { proxy in
+                    Circle()
+                        .frame(width: 50, height: 50)
+                        .foregroundColor(.orange)
+                        .position(x: self.move ? (proxy.size.width - 40) : 40, y: 40)
+                        .animation(.easeOut(duration: 2))
+                }
+            }
+            
+            Button("Change") {
+                self.move.toggle()
+            }
+        }
+        .font(.largeTitle)
+        .padding(.bottom)
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        //ContentView()
-        //RefactoringView()
-        //ShapesView()
-        VerticalStackView()
+        Group {
+            //ContentView()
+            //RefactoringView()
+            //ShapesView()
+            //VerticalStackView()
+            //HorizontalStackView()
+            //AlignmentHStackView()
+            //HStackPriorityView()
+            //IntroZStackView()
+            //BasicChangedCircle()
+            CurvesAnimationsView()
+        }
     }
 }
